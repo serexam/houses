60fe0d472a6b45c8a7086a7f4466057225f35dad
serexam <serexam@yandex.ru>
2018-10-07 23:05:53 +0300

    ooo

--- houses/views.py
+++ houses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import House
 
 def index(request):
-	return render(request, 'houses/index.html')
+	xyz = House.objects.all()
+	return render(request, 'houses/index.html', {'xyz': xyz})
